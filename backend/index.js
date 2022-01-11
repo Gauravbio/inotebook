c8edfd41f06457a4e4052919ac451f4423ee1866
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2022-01-11 17:11:03 +0530

    10 jan 2022

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const port=5000
 
 app.use(express.json());
 app.use('/api/auth',require('./routes/auth'))
-// app.use('/api/auth',require('./routes.notes'))
+app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
     console.log(`example at http://localhost:${port}`)
 })
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,54 @@
-const express =require ('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
+var fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
 
-router.get('/',(req,res) => {
-    
-    res.json([])
-})
+// ROUTE: 1: Get get all the notes using get"api/auth/getuser". login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("internal server error");
+  }
+});
+
+// ROUTE: 2: add a new note using post"api/notes/addnote". login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "enter a valid title").isLength({ min: 3 }),
+    body(
+      "description",
+      "enter a valid description minimum of 5 characters"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //if there are errors return bad requestand the errors
+      const errors = validationResult(req);
+      //after validations checking errors
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.id,
+      });
+      //saving note in json format in database
+      const savednote = await note.save();
+      
+      res.json(savednote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("internal server error");
+    }
+  }
+);
 module.exports = router;
